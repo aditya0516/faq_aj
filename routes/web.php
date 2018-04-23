cba2328082c6cf4179af103375358a85d20bbbb5
aditya <avj22@njit.edu>
2018-04-23 17:37:35 -0400

    Finished with Views as per the videos

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
 Route::get('/question/{question_id}/answer/{answer_id}', 'AnswerController@show')->name('answer.show');
